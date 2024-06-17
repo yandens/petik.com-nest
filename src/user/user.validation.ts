d2bd166ea7b49e87cc98d6bf1e995debf10c6c2b
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-17 20:53:47 +0800

    feat: Add create user bio validation

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,1 +1,13 @@
-export class UserValidation {}
+import { z, ZodType } from 'zod';
+
+export class UserValidation {
+  static readonly CREATE_USER_BIO: ZodType = z.object({
+    first_name: z.string().min(1),
+    last_name: z.string().min(1).optional(),
+    phone_number: z.string().min(12),
+    street: z.string().min(1),
+    city: z.string().min(1),
+    province: z.string().min(1),
+    country: z.string().min(1),
+  });
+}
