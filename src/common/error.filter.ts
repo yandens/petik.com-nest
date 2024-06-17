875dec9c5c58d1235c85dd3193f39cf9f67c22fb
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-17 20:51:21 +0800

    feat: Add error filter for jwt

--- src/common/error.filter.ts
+++ src/common/error.filter.ts
@@ -5,8 +5,9 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { ZodError } from 'zod';
+import { JsonWebTokenError, TokenExpiredError } from '@nestjs/jwt';
 
-@Catch(HttpException, ZodError)
+@Catch(HttpException, ZodError, JsonWebTokenError, TokenExpiredError)
 export class ErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
@@ -17,7 +18,14 @@ export class ErrorFilter implements ExceptionFilter {
       });
     } else if (exception instanceof ZodError) {
       response.status(400).json({
-        errors: 'Validation Error!',
+        errors: exception.errors,
+      });
+    } else if (
+      exception instanceof JsonWebTokenError ||
+      exception instanceof TokenExpiredError
+    ) {
+      response.status(401).json({
+        errors: exception.message,
       });
     } else {
       response.status(500).json({
