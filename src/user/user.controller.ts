6cf70bb98449ee60657c5100ef1947dcb2b6dd94
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-17 20:54:42 +0800

    feat: Add create user bio API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserBioRequest, UserBioResponse } from '../model/user.model';
+import { Auth } from '../auth/auth.decorator';
+import { User } from '@prisma/client';
+import { WebResponse } from '../model/web.model';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  @HttpCode(201)
+  async createUserBio(
+    @Auth() user: User,
+    @Body() request: CreateUserBioRequest,
+  ): Promise<WebResponse<UserBioResponse>> {
+    const result = await this.userService.createUserBio(user, request);
+    return {
+      data: result,
+    };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { ValidationService } from '../common/validation.service';
 import { PrismaService } from '../common/prisma.service';
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { User } from '@prisma/client';
+import {
+  CreateUserBioRequest,
+  toUserBioResponse,
+  UserBioResponse,
+} from '../model/user.model';
+import { UserValidation } from './user.validation';
+import { RandomUuidUtil } from '../utils/random-uuid.util';
 
 @Injectable()
 export class UserService {
@@ -13,6 +20,7 @@ export class UserService {
     private validationService: ValidationService,
     private prismaService: PrismaService,
     @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private randomUuidUtil: RandomUuidUtil,
   ) {}
 
   async getUserById(id: string, includeQuery?: any): Promise<any> {
@@ -42,10 +50,7 @@ export class UserService {
     this.logger.info(`Get users with size ${size} and page ${page}`);
 
     // get users
-    return this.prismaService.user.findMany({
-      take: size,
-      skip: page,
-    });
+    return this.prismaService.user.findMany({ take: size, skip: page });
   }
 
   async createUser(data: any): Promise<User> {
@@ -53,9 +58,7 @@ export class UserService {
     this.logger.info(`Create user with data ${JSON.stringify(data)}`);
 
     // create user
-    return this.prismaService.user.create({
-      data,
-    });
+    return this.prismaService.user.create({ data });
   }
 
   async updateUser(id: string, data: any): Promise<User> {
@@ -65,9 +68,40 @@ export class UserService {
     );
 
     // update user
-    return this.prismaService.user.update({
-      where: { id },
-      data,
+    return this.prismaService.user.update({ where: { id }, data });
+  }
+
+  async createUserBio(
+    user: User,
+    request: CreateUserBioRequest,
+  ): Promise<UserBioResponse> {
+    // log the request
+    this.logger.info(`Create user bio with data ${JSON.stringify(request)}`);
+
+    // validate the request
+    const createRequest: CreateUserBioRequest = this.validationService.validate(
+      UserValidation.CREATE_USER_BIO,
+      request,
+    );
+
+    // check user if exists
+    const userRecord = await this.getUserById(user.id);
+    if (!userRecord) throw new HttpException('User not found', 404);
+
+    // check if user bio exists
+    const userBioRecord = await this.prismaService.userBiodata.findFirst({
+      where: { user_id: user.id },
     });
+    if (userBioRecord) throw new HttpException('User bio already exists', 400);
+
+    // create user bio
+    const data = {
+      ...createRequest,
+      user_id: user.id,
+      id: this.randomUuidUtil.generateRandomId(),
+    };
+    const userBio = await this.prismaService.userBiodata.create({ data });
+
+    return toUserBioResponse(userBio);
   }
 }
