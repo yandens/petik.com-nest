c51b3bd2bf056b8aa66aca7adaaa67f8cb277378
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-22 21:38:26 +0800

    refactor: Update project structure

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -1,5 +1,5 @@
 import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
-import { PrismaService } from '../common/prisma.service';
+import { PrismaService } from '../service/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
